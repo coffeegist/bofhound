3000d16b105c446b16090f12ec79d245b96cad98
Marc André Tanner <mat@brain-dump.org>
2024-11-11 22:23:12 +0100

    Improve parsing of target domain SID for domain trusts

--- bofhound/ad/models/bloodhound_domaintrust.py
+++ bofhound/ad/models/bloodhound_domaintrust.py
@@ -3,6 +3,7 @@ from bofhound.ad.models.bloodhound_object import BloodHoundObject
 from bloodhound.ad.utils import ADUtils
 from bloodhound.ad.trusts import ADDomainTrust
 from bofhound.ad.helpers import TrustType, TrustDirection
+from impacket.ldap.ldaptypes import LDAP_SID
 import logging
 
 class BloodHoundDomainTrust(object):
@@ -23,27 +24,19 @@ class BloodHoundDomainTrust(object):
         self.TrustProperties = None
 
         if 'distinguishedname' in object.keys() and 'trustpartner' in object.keys() and \
-            'trustdirection' in object.keys() and 'trusttype' in object.keys() and 'trustattributes' in object.keys():
+            'trustdirection' in object.keys() and 'trusttype' in object.keys() and 'trustattributes' in object.keys() and \
+            'securityidentifier' in object.keys():
             
             self.LocalDomainDn = BloodHoundObject.get_domain_component(object.get('distinguishedname')).upper()
             trust_partner = object.get('trustpartner').upper()
             domain = ADUtils.ldap2domain(object.get('distinguishedname')).upper()
             logging.debug(f'Reading trust relationship between {ColorScheme.domain}{domain}[/] and {ColorScheme.domain}{trust_partner}[/]', extra=OBJ_EXTRA_FMT)
-            trust = ADDomainTrust(trust_partner, int(object.get('trustdirection')), object.get('trusttype'), int(object.get('trustattributes')), '')
+            domainsid = LDAP_SID()
+            domainsid.fromCanonical(object.get('securityidentifier'))
+            trust = ADDomainTrust(trust_partner, int(object.get('trustdirection')), object.get('trusttype'), int(object.get('trustattributes')), domainsid.getData())
             self.TrustProperties = trust.to_output()
 
             # BHCE now wants trusttype and direction defined as string names instead of int values
             
             self.TrustProperties['TrustDirection'] = TrustDirection(self.TrustProperties['TrustDirection']).name
             self.TrustProperties['TrustType'] = TrustType(self.TrustProperties['TrustType']).name
-
-    # Leaving the sid property blank, or setting it to a static value causes 
-    # BloodHound to improperly display trusts. Each trusted domain seems to 
-    # require a unique SID
-    def set_temporary_sid(self, indx):
-        self.TrustProperties['TargetDomainSid'] = f'S-1-5-21-{indx}'
-
-
-
-
-
