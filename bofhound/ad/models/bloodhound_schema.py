8e671168866c05a3f9cdff1ddf6750bcea18a65c
Marc André Tanner <mat@brain-dump.org>
2025-04-25 17:34:12 +0200

    Allow schemaIDGUID to be formatted as UUID
    This allows parsing of AD explorer originated LDIF output without a
    round trip through base64. The base64 encoded version remains supported,
    for usage with other tools.

--- bofhound/ad/models/bloodhound_schema.py
+++ bofhound/ad/models/bloodhound_schema.py
@@ -12,7 +12,11 @@ class BloodHoundSchema(object):
 		if 'name' in object.keys() and 'schemaidguid' in object.keys():
 			self.Name = object.get('name').lower()
 			try:
-				self.SchemaIdGuid = str(UUID(bytes_le=base64.b64decode(object.get('schemaidguid')))).lower()
+				value = object.get('schemaidguid')
+				if '-' in value:
+					self.SchemaIdGuid = value.lower()
+				else:
+					self.SchemaIdGuid = str(UUID(bytes_le=base64.b64decode(value))).lower()
 				logging.debug(f"Reading Schema object {ColorScheme.schema}{self.Name}[/]", extra=OBJ_EXTRA_FMT)
 			except:
 				logging.warning(f"Error base64 decoding SchemaIDGUID attribute on Schema {ColorScheme.schema}{self.Name}[/]", extra=OBJ_EXTRA_FMT)
