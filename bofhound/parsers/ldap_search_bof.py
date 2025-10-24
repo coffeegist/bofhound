6803c7cf5aaca9d40c27e90e5429ed9a227fa851
Adam <1392689+coffeegist@users.noreply.github.com>
2025-10-24 15:08:34 -0500

    * Implement BoundaryBasedParser parent class

--- bofhound/parsers/ldap_search_bof.py
+++ bofhound/parsers/ldap_search_bof.py
@@ -1,14 +1,16 @@
 """Implementation of ToolParser for ldapsearch BOF logs."""
-from .types import ObjectType, LdapRecordParser
+import re
+from typing_extensions import override
+from .types import ObjectType, BoundaryBasedParser
 
 
-class LdapSearchBofParser(LdapRecordParser):
+class LdapSearchBofParser(BoundaryBasedParser):
     """
     Implementation of ToolParser for ldapsearch BOF logs.
     """
 
     def __init__(self):
-        super().__init__(boundary_pattern="-" * 20)
+        super().__init__(start_boundary_pattern="-" * 20)
 
         self._skippable_patterns = [
             r'^\d{2}\/\d{2} \d{2}:\d{2}:\d{2} UTC \[output\]$',
@@ -23,3 +25,16 @@ class LdapSearchBofParser(LdapRecordParser):
     @property
     def produces_object_type(self) -> ObjectType:
         return ObjectType.LDAP_OBJECT
+
+    def _is_end_of_tool_output(self, line: str) -> bool:
+        """Check if the line indicates the end of tool output."""
+        return re.match(self._end_of_tool_output_pattern, line) is not None
+
+    @override
+    def process_line(self, line: str) -> None:
+        """Process a single line."""
+        line = line.strip()
+        if self._is_end_of_tool_output(line):
+            self._handle_end_boundary_line()
+            return
+        super().process_line(line)
